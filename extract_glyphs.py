f99f60f7ab29a186181cb090b6cd4be60e835fb7
Artyom Kolesnikov <artyom.s.kolesnikov@gmail.com>
2023-01-09 13:43:09 +0200

    Added "pixel bleeding" to remove black artefacts on the glyph edge

--- extract_glyphs.py
+++ extract_glyphs.py
@@ -1,8 +1,47 @@
 from fontTools.ttLib import TTFont
 from PIL import Image, ImageFont, ImageDraw
+import operator
 
 fontPath = "FirefoxEmoji.ttf"
 size = 100
+
+def bleed(img):
+    pixels = img.load()
+    edge = set()
+    width = img.size[0]
+    height = img.size[1]
+    for x in range(width):
+        for y in range(height):
+            if pixels[x, y][3] == 255:
+                for i in range(max(0, x - 1), min(x + 2, width)):
+                    for j in range(max(0, y - 1), min(y + 2, height)):
+                        alpha = pixels[i, j][3]
+                        if alpha > 0 and alpha < 255:
+                            edge.add((i, j))
+
+    for (x, y) in edge:
+        color = (0, 0, 0, 0)
+        count = 0
+        for i in range(max(0, x - 1), min(x + 2, width)):
+            for j in range(max(0, y - 1), min(y + 2, height)):
+                if (pixels[i, j][3] == 255):
+                    color = tuple(map(operator.add, color, pixels[i, j]))
+                    count += 1
+
+        if count > 0:
+            color = tuple(map(operator.floordiv, color[:3], (count, count, count)))
+            color = color + (255,)
+            pixels[x, y] = color
+
+    return len(edge) > 0
+
+def clear(img):
+    pixels = img.load()
+    width = img.size[0]
+    height = img.size[1]
+    for x in range(width):
+        for y in range(height):
+            pixels[x, y] = pixels[x, y][:3] + (0,)
 
 with TTFont(fontPath, fontNumber=0) as ttfont:
     imagefont = ImageFont.truetype(fontPath, size)
@@ -27,6 +66,10 @@ with TTFont(fontPath, fontNumber=0) as ttfont:
         except OSError:
             print(f"{text} -> error")
             continue
+
+        while bleed(img): pass
+        clear(img)
+        d.text((0, 0), text, font=imagefont, embedded_color=True)
 
         print(f"{text} -> {filename}")
         img.save(filename)
